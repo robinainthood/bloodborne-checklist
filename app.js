a8c9119a3b336eefc477ad461efdb019b9f12e43
robinainthood <112138589+robinainthood@users.noreply.github.com>
2026-05-08 12:16:30 -0400

    Split files

---
+++ app.js
@@ -0,0 +1,6 @@
+var profilesKey = 'bloodborne_checklist_profiles';
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-10706075-12');
